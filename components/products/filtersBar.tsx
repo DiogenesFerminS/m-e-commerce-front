e73b902c35f9c76f3ec4f359059ddad476093d34
Diogenes <paolofersantella@gmail.com>
2026-03-29 15:41:06 -0400

    filters init

--- components/products/filtersBar.tsx
+++ components/products/filtersBar.tsx
@@ -1,20 +1,8 @@
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 const FiltersBar = () => {
   return (
     <div className="px-6 py-4 mx-6">
-      <Select>
-        <SelectTrigger className="w-45">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+
     </div>
   );
 };
