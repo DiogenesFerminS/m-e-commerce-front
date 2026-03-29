417121697477a371be45170133899fec36246bb9
Diogenes <paolofersantella@gmail.com>
2026-03-29 15:21:31 -0400

    feat: initial commit

--- components/products/filtersBar.tsx
+++ components/products/filtersBar.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 const FiltersBar = () => {
   return (
-    <div className='bg-stone-900 px-6 py-4 mx-6'>
-        <span>asd</span>
+    <div className="px-6 py-4 mx-6">
+      <Select>
+        <SelectTrigger className="w-45">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
-  )
-}
+  );
+};
 
-export default FiltersBar
+export default FiltersBar;
